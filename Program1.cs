e53fc5bcf09c0dabd7d60ef1d1bf30f2c8a0962b
ndphuong18i1 <48077524+ndphuong18i1@users.noreply.github.com>
2019-03-22 12:27:26 +0700

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program1
+    {
+            static void Main(string[] args)
+            {
+
+                int n;
+                Console.WriteLine("Nhap so sinh vien la");
+                n = int.Parse(Console.ReadLine());
+                // tao mang doi tuong Sv
+                quanlysinhvien[] sv = new quanlysinhvien[n];
+                for (int i = 0; i < n; i++)
+                {
+                    sv[i] = new quanlysinhvien();
+                    sv[i].nhapTT();
+
+
+
+
+                }
+                Console.WriteLine("Danh sach Sinh Vien");
+                for (int j = 0; j < n; j++)
+                {
+                    sv[j].In();
+                }
+                Console.ReadKey();
+
+
+                quanlysinhvien tmp;
+                tmp = sv[0];
+                {
+                    for (int i = 0; i < n - 1; i++)
+                    {
+                        for (int j = i + 1; j < n; j++)
+
+                            if (sv[i].diemtb() < sv[j].diemtb())
+                            {
+                                tmp = sv[i];
+                                sv[i] = sv[j];
+                                sv[j] = tmp;
+                            }
+                    }
+                }
+
+                Console.WriteLine("sinh vien sap xep theo thu tu giam dan");
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("SV co diem tb {0} ", sv[i].diemtb());
+                }
+                Console.ReadLine();
+
+
+                Console.WriteLine("Nhap ten  sinh vien can tim");
+                string p = Console.ReadLine();
+                for (int i = 0; i < n; i++)
+                    if (sv[i].tenSV.Contains(p))
+                        Console.WriteLine("Ten SV muon tim la: Ten: {0}, Ma SV: {1}, Diem TB: {2}  ", sv[i].tenSV, sv[i].maSV, sv[i].diemtb());
+
+
+
+
+
+
+                Console.ReadLine();
+
+
+
+
+            }
+        
+    }
+}
---
+++ quanlysinhvien.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class quanlysinhvien
+    {
+        public String maSV;
+        public String tenSV;
+        public float diemTB;
+
+        //default constructors
+        public quanlysinhvien()
+        {
+            this.maSV = "01";
+            this.tenSV = "Ngo duy phuong";
+            this.diemTB = (float)8.0;
+
+        }
+        //tham so hoa constructors
+        public quanlysinhvien(String maSV, String tenSV, float diemTB)
+        {
+            this.maSV = maSV;
+            this.tenSV = tenSV;
+            this.diemTB = diemTB;
+        }
+
+
+
+
+
+        public void nhapTT()
+
+        {
+
+
+
+            Console.Write("Nhap Ten Sinh Vien: ");
+            this.tenSV = Console.ReadLine();
+
+            Console.Write("Nhap ma Sinh Vien: ");
+            this.maSV = Console.ReadLine();
+
+            Console.Write("Nhap Diem Sinh Vien:");
+            this.diemTB = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("--------------");
+
+
+
+
+
+        }
+
+        public void In()
+        {
+            Console.WriteLine("Ma Sinh Vien: {0} ", this.maSV);
+            Console.WriteLine("Ten Sinh Vien: {0} ", this.tenSV);
+            Console.WriteLine("Diem TB Sinh Vien: {0} ", this.diemTB);
+            Console.WriteLine("\n--------------------------\n");
+
+        }
+
+        public float diemtb()
+        {
+            return this.diemTB;
+        }
+
+
+
+
+    }
+}
